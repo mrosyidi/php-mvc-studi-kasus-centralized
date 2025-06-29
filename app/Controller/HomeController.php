534589629890f142cc593ea1f4b66cad74abe0da
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-29 10:57:31 +0700

    add HomeController.php

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php 
+
+    namespace ProgrammerZamanNow\Belajar\PHP\MVC\Controller;
+
+    class HomeController
+    {
+        function index(): void 
+        {
+            echo "HomeController.index()";
+        }
+
+        function hello(): void 
+        {
+            echo "HomeController.hello()";
+        }
+
+        function world(): void 
+        {
+            echo "HemeController.world()";
+        }
+    }
