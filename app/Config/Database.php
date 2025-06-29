454691bb01744768302eb0692ec071914c3e9d41
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-29 21:34:27 +0700

    add Database.php

---
+++ app/Config/Database.php
@@ -0,0 +1,22 @@
+<?php 
+
+    namespace ProgrammerZamanNow\Belajar\PHP\MVC\Config;
+
+    class Database
+    {
+        private static ?\PDO $pdo = null;
+
+        public static function getConnection(string $env = "test"): \PDO 
+        {
+            if(self::$pdo == null)
+            {
+                require_once __DIR__ . "/../../config/database.php";
+                $config = getDatabaseConfig();
+                self::$pdo = new \PDo($config['database'][$env]['url'],
+                $config['database'][$env]['username'],
+                $config['database'][$env]['password']);
+            }
+
+            return self::$pdo;
+        }
+    }
