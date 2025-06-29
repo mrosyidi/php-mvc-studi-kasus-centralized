f41f917f8def78489399635936719a10060cd3a4
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-29 13:09:21 +0700

    update index.php menambahkan router produk

--- public/index.php
+++ public/index.php
@@ -4,7 +4,9 @@
 
     use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\ProductController;
 
+    Router::add('GET', '/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
     Router::add('GET', '/', HomeController::class, 'index');
     Router::add('GET', '/hello', HomeController::class, 'hello');
     Router::add('GET', '/world', HomeController::class, 'world');
