f4207bc7494e1bd2c896aaa385f44761cff534cf
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-29 10:11:11 +0700

    update Router.php memilih controller dari path info

--- app/App/Router.php
+++ app/App/Router.php
@@ -18,6 +18,22 @@
 
         public static function run(): void 
         {
+            $path = '/';
+            
+            if(isset($_SERVER['PATH_INFO'])) $path = $_SERVER['PATH_INFO'];
+            
+            $method = $_SERVER['REQUEST_METHOD'];
+            
+            foreach(self::$routes as $route)
+            {
+                if($path == $route['path'] && $method == $route['emthod'])
+                {
+                    echo "CONTROLLER : " . $route['controller'] . ", FUNCTION : " . $route['function'];
+                    return;
+                }
+            }
 
+            http_response_code(404);
+            echo "CONTROLLER NOT FOUND";
         }
     }
