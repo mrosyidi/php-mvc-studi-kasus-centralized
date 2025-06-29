42389ec907ab907cef3b31c3f23e4b7f5bfe79b1
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-29 11:03:12 +0700

    update index.php integrasi controller dengan router

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
     require_once __DIR__ . "/../vendor/autoload.php";
 
     use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
 
-    Router::add('GET', '/', 'HomeController', 'index');
-    Router::add('GET', '/login', 'UserController', 'login');
-    Router::add('GET', '/register', 'UserController', 'register');
+    Router::add('GET', '/', HomeController::class, 'index');
+    Router::add('GET', '/hello', HomeController::class, 'login');
+    Router::add('GET', '/world', HomeController::class, 'register');
     Router::run();
