22ea53e0ff5e7b2c9f889f2f44d31f894a60960d
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-30 11:07:26 +0700

    update HomeController.php mengubah controller home

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -8,38 +8,6 @@
     {
         function index(): void 
         {
-            $model = [
-                'title' => 'Belajar PHP MVC',
-                'content' => 'Selamat belajar PHP MVC di Channel Programmer Zaman Now'
-            ];
-
-            View::render('Home/index', $model);
-        }
-
-        function hello(): void 
-        {
-            echo "HomeController.hello()";
-        }
-
-        function world(): void 
-        {
-            echo "HemeController.world()";
-        }
-
-        function about(): void 
-        {
-            echo "Author, Eko Kurniawan Khannedy";
-        }
-
-        function login(): void 
-        {
-            $request = [
-                'username' => $_POST['username'],
-                'password' => $_POST['password']
-            ];
-
-            $response = [
-                'message' => 'Login sukses'
-            ];
+            View::render("Home/index", ["title" => "PHP Login Management"]);
         }
     }
