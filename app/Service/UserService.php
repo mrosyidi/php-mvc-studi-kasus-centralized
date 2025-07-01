6841acdc1aed39bec754d3507459f9630e8916b7
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-07-01 17:18:01 +0700

    update UserService.php menambah fitur login

--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -6,6 +6,8 @@
     use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterRequest;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterResponse;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserLoginRequest;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserLoginResponse;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Exception\ValidationException;
 
@@ -57,6 +59,35 @@
                 trim($request->id == "") || trim($request->name == "") || trim($request->password == ""))
             {
                 throw new ValidationException("Id, Name, Password can not blank");
+            }
+        }
+
+        public function login(UserLoginRequest $request): UserLoginResponse 
+        {
+            $this->validateUserLoginRequest($request);
+            $user = $this->userRepository->findById($request->id);
+
+            if($user == null)
+            {
+                throw new ValidationException("Id or password is wrong");
+            }
+
+            if(password_verify($request->password, $user->password))
+            {
+                $response = new UserLoginResponse();
+                $response->user = $user;
+                return $response;
+            }else 
+            {
+                throw new ValidationException("Id or password is wrong");
+            }
+        }
+
+        private function validateUserLoginRequest(UserLoginRequest $request)
+        {
+            if($request->id == null || $request->password == null || trim($request->id == "") || trim($request->password == ""))
+            {
+                throw new ValidationException("Id, Password can not blank");
             }
         }
     }
