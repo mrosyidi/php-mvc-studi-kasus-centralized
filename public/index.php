69f0b6c5ad24c7297e485cb09460183588f31be4
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-07-01 13:28:04 +0700

    update index.php menambah routing register

--- public/index.php
+++ public/index.php
@@ -3,8 +3,13 @@
     require_once __DIR__ . "/../vendor/autoload.php";
 
     use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\UserController;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\AuthMiddleware;
 
+    Database::getConnection('prod');
     Router::add('GET', '/', HomeController::class, 'index', []);
+    Router::add('GET', '/users/register', UserController::class, 'register', []);
+    Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
     Router::run();
