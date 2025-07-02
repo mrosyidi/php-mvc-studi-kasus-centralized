ac8c9e20f78fbca6f41443a62731913485665ba8
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-07-02 11:31:34 +0700

    update UserController.php menambah controller untuk login

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -7,6 +7,7 @@
     use ProgrammerZamanNow\Belajar\PHP\MVC\Service\UserService;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterRequest;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserLoginRequest;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Exception\ValidationException;
 
     class UserController 
@@ -39,6 +40,27 @@
             }catch(ValidationException $exception)
             {
                 View::render('User/register', ['title' => 'Register new User', 'error' => $exception->getMessage()]);
+            }
+        }
+
+        public function login()
+        {
+            View::render('User/login', ['title' => 'Login user']);
+        }
+
+        public function postLogin()
+        {
+            $request = new UserLoginRequest();
+            $request->id = $_POST['id'];
+            $request->password = $_POST['password'];
+
+            try 
+            {
+                $this->userService->login($request);
+                View::redirect('/');
+            }catch(ValidationException $exception)
+            {
+                View::render('User/login', ['title' => 'Login user', 'error' => $exception->getMessage()]);
             }
         }
     }
