1c14b99e16f91238fa6cabf60992837df013d6ec
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-07-03 12:43:25 +0700

    update index.php menambah middleware

--- public/index.php
+++ public/index.php
@@ -6,13 +6,14 @@
     use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
     use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\UserController;
-    use ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\AuthMiddleware;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\MustNotLoginMiddleware;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\MustLoginMiddleware;
 
     Database::getConnection('prod');
     Router::add('GET', '/', HomeController::class, 'index', []);
-    Router::add('GET', '/users/register', UserController::class, 'register', []);
-    Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
-    Router::add('GET', '/users/login', UserController::class, 'login', []);
-    Router::add('POST', '/users/login', UserController::class, 'postLogin', []);
-    Router::add('GET', 'users/logout', UserController::class, 'logout', []);
+    Router::add('GET', '/users/register', UserController::class, 'register', [MustNotLoginMiddleware::class]);
+    Router::add('POST', '/users/register', UserController::class, 'postRegister', [MustNotLoginMiddleware::class]);
+    Router::add('GET', '/users/login', UserController::class, 'login', [MustNotLoginMiddleware::class]);
+    Router::add('POST', '/users/login', UserController::class, 'postLogin', [MustNotLoginMiddleware::class]);
+    Router::add('GET', '/users/logout', UserController::class, 'logout', [MustLoginMiddleware::class]);
     Router::run();
