2c58132170ae5a44b3dbd5e66ae96b5de35d67a0
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-07-03 07:59:56 +0700

    update HomeController.php menambah login process

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -3,11 +3,33 @@
     namespace ProgrammerZamanNow\Belajar\PHP\MVC\Controller;
 
     use ProgrammerZamanNow\Belajar\PHP\MVC\App\View;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\SessionRepository;
+    use ProgrammerZamanNow\Belajar\PHP\MVC\Service\SessionService;
 
     class HomeController
     {
+        private SessionService $sessionService;
+
+        public function __construct()
+        {
+            $connection = Database::getConnection();
+            $sessionRepository = new SessionRepository($connection);
+            $userRepository = new UserRepository($connection);
+            $this->sessionService = new SessionService($sessionRepository, $userRepository);
+        }
+
         function index(): void 
         {
-            View::render("Home/index", ["title" => "PHP Login Management"]);
+            $user = $this->sessionService->current();
+
+            if($user == null)
+            {
+                View::render("Home/index", ["title" => "PHP Login Management"]);
+            }else 
+            {
+                View::render('Home/dashboard', ['title' => 'Dashboard', 'user' => ['name' => $user->name]]);
+            }
         }
     }
